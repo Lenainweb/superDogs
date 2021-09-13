86001986cc8168bb589c7821b3ea3b1e1d13581b
Lenainweb <Lenainweb@gmail.com>
2021-09-13 18:53:04 +0300

    fixing gitignore

---
+++ superdog/registration_of_dogs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RegistrationOfDogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'registration_of_dogs'
---
+++ superdog/registration_of_dogs/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+from django.db import models
+
+class Bd(models.Model):
+    pass
