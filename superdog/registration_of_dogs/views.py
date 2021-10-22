a2c7f6520bba144f44da4365bd8774f21edb65f5
Lenainweb <Lenainweb@gmail.com>
2021-10-23 01:26:59 +0300

    forms.py

--- superdog/registration_of_dogs/views.py
+++ superdog/registration_of_dogs/views.py
@@ -33,28 +33,67 @@ class RegistrationExhibitionView(TemplateView):
         form_dog = DogRegistrationExhibitionForm(request.POST, request.FILES)
         form_owner = OwnerRegistrationExhibitionForm(request.POST)
         
-        if form_owner.is_valid():
-            print("form_owner:")
-            print(form_owner.cleaned_data)
-        else:
-            print("form_owner: errors:")
-            print(form_owner.errors)
+        if form_owner.is_valid() and form_registr.is_valid() and form_dog.is_valid():
 
-        if form_registr.is_valid():
-            print("form_registr:")
-            print(form_registr.cleaned_data)
-            messages.info(request, 'Your form_registr successfully!')
+            # new_owner = Owner(form_owner.cleaned_data)
+            # new_owner.save()
+            owner = form_owner.save(commit=False)
+            owner.save()
 
-        else:
-            print("form_registr: errors:")
-            print(form_registr.errors)
-        
-        if form_dog.is_valid():
             print("form_dog:")
             print(form_dog.cleaned_data)
+
+            # date_of_birth = form_dog.cleaned_data["date_of_birth"]
+
+            dog = form_dog.save(commit=False)
+            dog.date_of_birth = form_dog.cleaned_data["date_of_birth"]
+            dog.name_of_owner = owner
+            dog.save()
+            dog_id = dog.id
+
+            # register = form_registr
+            # register.dog_id = dog_id #Dog.objects.filter(dog=dog) 
+            # register.save()
+
+
+
+            messages.success(request, 'Your forms successfully!')
+
         else:
-            print("form_dog: errors:")
-            print(form_dog.errors)
+            messages.error(request, form_owner.errors, form_registr.errors, form_dog.errors)
+        
+        # if form_owner.is_valid():
+        #     print("form_owner:/n", form_owner.cleaned_data)
+        #     messages.success(request, 'Your form_owner successfully!')
+        #     form_owner.save(commit=False)
+        # else:
+        #     print("form_owner: errors:")
+        #     print(form_owner.errors)
+        #     messages.error(request, form_owner.errors)
+
+        # if form_registr.is_valid():
+        #     print("form_registr:/n", form_registr.cleaned_data)
+        #     messages.success(request, 'Your form_registr successfully!')
+        #     form_registr.save(commit=False)
+
+        # else:
+        #     print("form_registr: errors:")
+        #     print(form_registr.errors)
+        #     messages.error(request, form_registr.errors)
+        
+        # if form_dog.is_valid():
+        #     print("form_dog:")
+        #     print(form_dog.cleaned_data)
+        #     messages.success(request, 'Your form_dog successfully!')
+            
+        #     form_dog.save(commit=False)
+            
+        #     print("SAVE")
+        # else:
+        #     print("form_dog: errors:")
+        #     print(form_dog.errors)
+        #     messages.error(request, form_dog.errors)
+
 
         return redirect('/')
         
