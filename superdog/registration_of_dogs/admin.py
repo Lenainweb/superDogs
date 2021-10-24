91446cebf525fc4ed711c90346fede894cd43486
Lenainweb <Lenainweb@gmail.com>
2021-10-25 01:05:47 +0300

    add admin view

--- superdog/registration_of_dogs/admin.py
+++ superdog/registration_of_dogs/admin.py
@@ -6,9 +6,9 @@ from .models import AboutUs, Exebition, AdditionalCategories, Owner, Dog, Regist
 # admin.site.register(Exebition)
 # admin.site.register(AdditionalCategories)
 # admin.site.register(Owner)
-admin.site.register(Dog)
-admin.site.register(RegistrationExhibition)
-admin.site.register(Fees)
+# admin.site.register(Dog)
+# admin.site.register(RegistrationExhibition)
+# admin.site.register(Fees)
 
 
 
@@ -26,10 +26,24 @@ class AdditionalCategoriesAdmin(ModelAdmin):
 class OwnerAdmin(ModelAdmin):
     list_display = ('owner_name', 'owner_telephone', 'owner_email', 'display_dogs', 'status_of_user')
 
+class DogAdmin(ModelAdmin):
+    list_display = ('breed_race', 'name_of_owner', 'name_of_dog', 'date_of_birth')
+
+class RegistrationExhibitionAdmin(ModelAdmin):
+    list_display = ('exebition', 'dog', 'class_of_exebition')
+
+class FeesAdmin(ModelAdmin):
+    list_display = ('position', 'price')
+
 admin.site.register(AboutUs, AboutUsAdmin)
 admin.site.register(Exebition, ExebitionAdmin)
 admin.site.register(AdditionalCategories, AdditionalCategoriesAdmin)
 admin.site.register(Owner, OwnerAdmin)
+admin.site.register(Dog, DogAdmin)
+admin.site.register(RegistrationExhibition, RegistrationExhibitionAdmin)
+admin.site.register(Fees, FeesAdmin )
+
+
 
 
 
